15e992db506bd0083e1f122164051e6573d72569
AMAN SHARMA <sharmaaman.2099@gmail.com>
2018-07-06 15:41:44 +0530

    Add files via upload

---
+++ blnk.ino
@@ -0,0 +1,29 @@
+void setup() {
+  pinMode(9, INPUT);
+  pinMode(4, OUTPUT);
+
+}
+int i = 0;
+void loop() {
+
+  if ((digitalRead(9) == HIGH && i == 0) )
+  {
+    digitalWrite(4, HIGH);
+    i = 1;
+  }
+  else if (digitalRead(9) == LOW && i == 1)
+  {
+    digitalWrite(4, HIGH);
+    i = 2;
+  }
+  else if ((digitalRead(9) == HIGH && i == 2))
+  {
+    digitalWrite(4, LOW);
+    i = 3;
+  }
+  else if ((digitalRead(9) == LOW && i == 3))
+  {
+    digitalWrite(4, LOW);
+    i = 0;
+  }
+}
